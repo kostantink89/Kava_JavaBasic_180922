aae230ef49fd81300d6e739c9ff6e111f0749926
Kostantin Kava <110024628+kostantink89@users.noreply.github.com>
2022-09-18 17:42:12 +0300

    Quick changes to Guess Number HomeWork

--- GuessNumber.java
+++ GuessNumber.java
@@ -8,13 +8,19 @@ public class GuessNumber {
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter your number from 1 to 6: ");
         int number = scan.nextInt();
-        double randomNumber = (Math.random() * 6)+1;
+        if (number > 6) {
+            System.out.println("Sorry,you have entered incorrect value.Please try again.");
+            System.exit(0);
+        }
+        double randomNumber = (Math.random() * 6) + 1;
         int integer = (int) randomNumber;
+
 
         if (number == integer) {
             System.out.println("Congratulations! You got it!");
         } else {
             System.out.println("Sorry you missed.The guessed number was: " + integer);
+
         }
 
 
